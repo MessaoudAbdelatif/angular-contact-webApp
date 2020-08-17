621e5c43374a45453a68af68ba5018f6b75a2828
Messaoud <dzdevsoft@gmail.com>
2020-08-17 12:06:13 +0200

    add login component and connect to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,23 @@ import {HeaderComponent} from './header/header.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {LoginComponent} from './auth/login/login.component';
+import {NgxWebstorageModule} from "ngx-webstorage";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    NgxWebstorageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,17 +2,31 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SignupRequestPayload} from "../signup/signup-request.payload";
 import {Observable} from "rxjs";
+import {LoginResponse} from "../login/login.response.payload";
+import {LoginRequest} from "../login/login.request.payload";
+import {map} from "rxjs/operators";
+import {LocalStorageService} from 'ngx-webstorage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private localStorage: LocalStorageService) {
   }
 
   signup(signupRequestPayload: SignupRequestPayload): Observable<any> {
     return this.httpClient.post('http://localhost:8080/api/auth/signup', signupRequestPayload, {responseType: 'text'})
+  }
 
+  // @ts-ignore
+  login(loginRequest: LoginRequest): Observable<boolean> {
+    this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/login', loginRequest)
+      .pipe(map(data => {
+      this.localStorage.store('authenticationToken', data.authenticationToken);
+      this.localStorage.store('username', data.username);
+      this.localStorage.store('expiresAt', data.expiresAt);
+      return true;
+      }));
   }
 }
